5657ac6615d5f993bad86c62e989dc2dd1ef0f77
idevmans <man1080@yandex.ru>
2021-01-18 10:31:22 +0400

    fix

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ sudo apt-get install mc arj dbview libaspell-dev links python zip bash-completio
 sudo apt-get install gcc g++ make -y
 sudo apt-get install mysql-client mysql-common mysql-server -y 
 sudo apt-get install composer git -y
-sudo apt-get install php7.4 php$phpVer-bcmath php$phpVer-bz2 php$phpVer-cli php$phpVer-common php$phpVer-curl php$phpVer-dba php$phpVer-dev php$phpVer-enchant php$phpVer-fpm php$phpVer-gd php$phpVer-gmp php$phpVer-imap php$phpVer-intl php$phpVer-json php$phpVer-mbstring php$phpVer-mysql php$phpVer-opcache php$phpVer-phpdbg php$phpVer-pspell php$phpVer-readline php$phpVer-soap php$phpVer-tidy php$phpVer-xml php$phpVer-xmlrpc php$phpVer-xsl php$phpVer-zip whois -y
+sudo apt-get install php$phpVer php$phpVer-bcmath php$phpVer-bz2 php$phpVer-cli php$phpVer-common php$phpVer-curl php$phpVer-dba php$phpVer-dev php$phpVer-enchant php$phpVer-fpm php$phpVer-gd php$phpVer-gmp php$phpVer-imap php$phpVer-intl php$phpVer-json php$phpVer-mbstring php$phpVer-mysql php$phpVer-opcache php$phpVer-phpdbg php$phpVer-pspell php$phpVer-readline php$phpVer-soap php$phpVer-tidy php$phpVer-xml php$phpVer-xmlrpc php$phpVer-xsl php$phpVer-zip whois -y
 sudo apt install nginx nginx-extras -y
 echo "Php7.4,composer,git,mysql,nginx have been installed"
 sleep 1
