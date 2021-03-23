c45348a78590858a15e51bd2adb80ed012885f4b
idevmans <man1080@yandex.ru>
2021-03-23 13:51:17 +0400

    A few not necessary changes

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ Inspired by https://gist.github.com/irazasyed/a7b0a079e7727a4315b9
 echo "update 0.0.18"
 read -p "Choose php version 5.6 to 8.0, recommend 7.4 : " phpVer
 echo "You choosed php $phpVer";
-echo "Before Installtion start please type your password for install all software as root"
+echo "Before Installtion please type your password for install all software as root"
 sleep 1
 
 sudo apt install software-properties-common curl -y
@@ -37,14 +37,14 @@ sudo apt-get install mysql-client mysql-common mysql-server -y
 sudo apt-get install composer git -y
 sudo apt-get install php$phpVer php$phpVer-bcmath php$phpVer-bz2 php$phpVer-cli php$phpVer-common php$phpVer-curl php$phpVer-dba php$phpVer-dev php$phpVer-enchant php$phpVer-fpm php$phpVer-gd php$phpVer-gmp php$phpVer-imap php$phpVer-intl php$phpVer-json php$phpVer-mbstring php$phpVer-mysql php$phpVer-opcache php$phpVer-phpdbg php$phpVer-pspell php$phpVer-readline php$phpVer-soap php$phpVer-tidy php$phpVer-xml php$phpVer-xmlrpc php$phpVer-xsl php$phpVer-zip whois -y
 sudo apt install nginx nginx-extras -y
-echo "Php7.4,composer,git,mysql,nginx have been installed"
+echo "Php,composer,git,mysql,nginx have been installed"
 sleep 1
 
-echo "Now node will be installed"
+echo "Now nodejs will be installed"
 sleep 1
-# //NVM provides you easy manage Node and NPM
+# //NVM provides you easy way to manage NodeJS and NPM version
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-echo "Restart terminal and do this command for install latest npm version nvm install node --lts"
+echo "Restart terminal and do this command for install latest NPM | nvm install node --lts"
 sleep 3
-echo "Installation compleate enjoy !"
+echo "Installation complete enjoy !"
 
